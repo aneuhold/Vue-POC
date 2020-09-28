04599babce350fea99d0c79f933b091ed6787181
Anton G Neuhold Jr <agneuhold@gmail.com>
2020-09-27 17:29:51 -0700

    Add basic changes to the site

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="This is a Vue.js app"/>
+    <p>
+      {{message}}
+    </p>
   </div>
 </template>
 
